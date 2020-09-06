275660e4432fe937b4b392a387e8afc9fe243bfd
Sunyu <33867159+Kaoruha@users.noreply.github.com>
2020-09-06 14:21:28 +0800

    Next Done

--- web/src/store/current_game/getters.js
+++ web/src/store/current_game/getters.js
@@ -1,6 +1,13 @@
-export default function getCurrentGameID(state) {
+export default function getCurrentGame(state) {
   if (state.current_game_id === 0) {
     state.current_game_id = localStorage.getItem('current_game_id')
   }
-  return state.current_game_id
+  if (state.current_rounds === 0) {
+    state.current_rounds = localStorage.getItem('current_rounds')
+  }
+  result = {
+    "current_game_id":state.current_game_id,
+    "current_rounds":state.current_rounds,
+  }
+  return result
 }
--- web/src/store/current_game/mutations.js
+++ web/src/store/current_game/mutations.js
@@ -2,3 +2,8 @@ export function updateCurrentGameID(state, gameID) {
   state.current_game_id = gameID
   localStorage.setItem('current_game_id', gameID)
 }
+
+export function updateCurrentRounds(state, rounds) {
+  state.current_rounds = rounds
+  localStorage.setItem('current_rounds', rounds)
+}
