10bec28c3a6c7dfd2186acb4aa17f7bef1fb6c36
Yu Sun <809186577@qq.com>
2020-09-15 17:31:07 +0800

    Update test.py

--- app/api/test.py
+++ app/api/test.py
@@ -10,7 +10,9 @@ from ..authorization.token_auth import creat_token, verify_token
 yp_test = YellowPrint('yp_test', url_prefix='/test')
 
 
-@yp_test.route('/test', methods=['POST'])
+@yp_test.route('/test', methods=['GET'])
 # @login_required
 def test():
+    t = request.args['co_num']
+    print(t)
     return NoException(msg='NoProblem')
